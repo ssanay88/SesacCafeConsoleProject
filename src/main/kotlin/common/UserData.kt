e58ac6f1ce291a3ebe44849e8b96654e9773b2e0
yangsuhyuck <pplloo748@gmail.com>
2025-05-23 15:32:44 +0900

    fix/UserData 패스워드 부분 val -> var 변경

--- src/main/kotlin/common/UserData.kt
+++ src/main/kotlin/common/UserData.kt
@@ -3,8 +3,7 @@ package common
 data class UserData(
     val name: String,
     val id: String,
-
-    val password: String,
+    var password: String,
     var balance: Int = 0,
     val orderHistory: MutableList<Order> = mutableListOf(),
     var stamp: Int = 0
