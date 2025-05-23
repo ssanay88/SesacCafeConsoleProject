a759d9ecbc58d15ab1699c362ff117430c4719dc
yangsuhyuck <pplloo748@gmail.com>
2025-05-23 15:22:45 +0900

    feature/로그인 페이지 개발 및 로그인한 유저의 정보에 접근할 싱글톤 객체 생성

---
+++ src/main/kotlin/AuthManager.kt
@@ -0,0 +1,104 @@
+import common.Order
+import common.UserData
+import database.UserDBManager
+
+/**
+ * Auth manager
+ * 로그인한 유저에 대한 정보를 처리할 클래스
+ */
+object AuthManager {
+    private var _currentUser: UserData? = null
+
+    val currentUser: UserData?
+        get() = _currentUser
+
+    fun login(user: UserData) {
+        _currentUser = user
+    }
+
+    fun logout() {
+        _currentUser = null
+    }
+
+    // 비밀번호 변경
+    fun setUserPw(newPw: String) {
+        val user = _currentUser
+        user?.let {
+            val updatedUser = user.copy(password = newPw)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("비밀번호가 변경되었습니다.")
+        }
+    }
+
+    // 잔액 충전
+    fun chargeMoney(money: Int) {
+        val user = _currentUser
+        user?.let {
+            val updatedUser = user.copy(balance = user.balance + money)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("금액이 충전되었습니다.")
+        }
+    }
+
+    // 결제 처리
+    fun makePayment(payMoney: Int) {
+        val user = _currentUser
+        user?.let {
+            val updatedUser = user.copy(balance = user.balance - payMoney)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("${payMoney}원 결제되었습니다.")
+        }
+    }
+
+    // 주문 내역 추가
+    fun addOrder(order: Order) {
+        val user = _currentUser
+        user?.let {
+            val updatedOrderList = user.orderHistory.apply { add(order) }
+            val updatedUser = user.copy(orderHistory = updatedOrderList)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("새로운 주문 내역이 추가되었습니다.")
+        }
+    }
+
+    // 스탬프 적립
+    fun addStamp(stampCnt: Int) {
+        val user = _currentUser
+        user?.let {
+            val updatedUser = user.copy(stamp = user.stamp + stampCnt)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("새로운 스탬프 ${stampCnt}개가 적립되었습니다. [현재 스탬프 : ${updatedUser.stamp}]")
+        }
+    }
+
+    fun useStamp(stampCnt: Int) {
+        val user = _currentUser
+        user?.let {
+            val updatedUser = user.copy(stamp = user.stamp - stampCnt)
+            UserDBManager.updateUser(updatedUser)
+            _currentUser = updatedUser
+            println("새로운 스탬프 ${stampCnt}개가 적립되었습니다. [현재 스탬프 : ${updatedUser.stamp}]")
+        }
+    }
+
+    fun isStampOver10(): Boolean {
+        return _currentUser?.stamp ?: 0 >= 10
+    }
+
+    fun printLoginUserInfo() {
+        println("""
+            이름 : ${_currentUser?.name}
+            ID : ${_currentUser?.id}
+            PW : ${_currentUser?.password}
+            Balance : ${_currentUser?.balance}
+            OrderHistory : ${_currentUser?.orderHistory}
+            Stamp : ${_currentUser?.stamp}
+        """.trimIndent())
+    }
+
+}
--- src/main/kotlin/EntryMain.kt
+++ src/main/kotlin/EntryMain.kt
@@ -1,4 +1,37 @@
+import login.LoginPage
+import signup.SignupPage
 
 fun main() {
+//    val newUser = UserData("양대리", "pplloo748", "q1w2e3r4")
+//    UserDBManager.addNewUser(newUser)
+    startSesacCafePG()
+}
+
+fun startSesacCafePG() {
+    println("[새싹 카페 주문 프로그램 실행]")
+    print("로그인을 원하시는 경우 1번, 신규 회원 가입을 원하시는 경우 2번을 입력해주세요. : ")
+    when (readln().trim().toInt()) {
+        1 -> startLoginPage()
+        2 -> startSignupPage()
+        else -> println("잘못된 입력입니다.")
+    }
+}
+
+fun startLoginPage() {
+    val loginPage = LoginPage()
+    loginPage.startLogin()
+    goToMainPage()
+}
+
+fun startSignupPage() {
+    val signuPage = SignupPage()
+    signuPage.startSignup()
+    goToMainPage()
+}
+
+fun goToMainPage() {
 
 }
+
+
+
