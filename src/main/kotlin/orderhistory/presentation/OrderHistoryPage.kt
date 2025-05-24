11e24acf640d48b751adce97a0f6d46e2aeb22bb
hjw9623 <hjw6191.wh@gmail.com>
2025-05-24 20:35:01 +0900

    fix: 주문 내역 조회 시 최신데이터 반영

--- src/main/kotlin/orderhistory/presentation/OrderHistoryPage.kt
+++ src/main/kotlin/orderhistory/presentation/OrderHistoryPage.kt
@@ -1,18 +1,26 @@
 package orderhistory.presentation
 
 import common.UserData
+import database.UserDBManager
 
 class OrderHistoryPage {
 
-    fun viewOrderHistory(user: UserData) {
+    fun startOrderHistoryPage(user: UserData) {
         println("\n|||| 주문 내역 조회 페이지 ||||")
 
-        if (user.orderHistory.isEmpty()) {
+        val lastestData = UserDBManager.findUserDataById(user.id)
+
+        if (lastestData == null) {
+            println("존재하지 않는 유저입니다.")
+            return
+        }
+
+        if (lastestData.orderHistory.isEmpty()) {
             println("주문 내역이 없습니다.")
             return
         }
 
-        user.orderHistory.forEachIndexed { index, order ->
+        lastestData.orderHistory.forEachIndexed { index, order ->
             println("[${index + 1}] 주문번호: ${order.orderId}")
             println("주문 날짜: ${order.orderDate}")
             println("주문 항목:")
