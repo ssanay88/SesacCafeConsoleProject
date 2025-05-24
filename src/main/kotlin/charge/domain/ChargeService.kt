1793b11f4897a0479e875090d1b8575a47916720
hjw9623 <hjw6191.wh@gmail.com>
2025-05-24 20:34:22 +0900

    feat: 충전 내역을 UserDb에 반영

--- src/main/kotlin/charge/domain/ChargeService.kt
+++ src/main/kotlin/charge/domain/ChargeService.kt
@@ -1,9 +1,11 @@
 package charge.domain
 
 import common.UserData
+import database.UserDBManager
 
 object ChargeService {
     fun charge(userData: UserData, amount: Int) {
         userData.balance += amount
+        UserDBManager.updateUser(userData)
     }
 }
