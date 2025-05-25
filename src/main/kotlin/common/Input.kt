0470eb659ed3f3e3d6f33e94ceaf0dac5f8ec20d
yangsuhyuck <pplloo748@gmail.com>
2025-05-25 18:55:47 +0900

    fix/입력 관련 공통 기능 파일로 변경

--- src/main/kotlin/common/Input.kt
+++ src/main/kotlin/common/Input.kt
@@ -1,15 +1,29 @@
 package common
 
-object Input {
+fun getStringInput(text: String): String {
+    print(text)
+    return readlnOrNull()?.trim() ?: ""
+}
 
-    fun getStringInput(text: String): String {
-        print(text)
-        return readlnOrNull()?.trim() ?: ""
+fun getIntInput(text: String): Int? {
+    val input = getStringInput(text)
+    return input.toIntOrNull()
+}
+
+fun inputZeroToMax(maxInput: Int): Int {
+    var input = readln().toInt()
+    while (input !in 0..maxInput) {
+        print("0 ~ ${maxInput}까지의 숫자를 입력해주세요 : ")
+        input = readln().toInt()
     }
+    return input
+}
 
-    fun getIntInput(text: String): Int? {
-        val input =getStringInput(text)
-        return input.toIntOrNull()
+fun inputOneToMax(maxInput: Int): Int {
+    var input = readln().toInt()
+    while (input !in 1..maxInput) {
+        print("1 ~ ${maxInput}까지의 숫자를 입력해주세요 : ")
+        input = readln().toInt()
     }
-
+    return input
 }
