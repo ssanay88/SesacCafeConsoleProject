8569051eb197951ba100cb7216bf3a599cb19646
hjw9623 <hjw6191.wh@gmail.com>
2025-05-26 20:34:59 +0900

    refactor: 주문 기록 조회 페이지 유저 null 체크 제거 및 하드코딩 제거

--- src/main/kotlin/orderhistory/presentation/OrderHistoryPage.kt
+++ src/main/kotlin/orderhistory/presentation/OrderHistoryPage.kt
@@ -1,33 +1,26 @@
 package orderhistory.presentation
 
 import common.UserData
-import database.UserDBManager
+import orderhistory.OrderHistoryMessage
 
 class OrderHistoryPage {
 
     fun startOrderHistoryPage(user: UserData) {
-        println("\n|||| 주문 내역 조회 페이지 ||||")
+        println(OrderHistoryMessage.ORDER_HISTORY_PAGE_TITLE)
 
-        val lastestData = UserDBManager.findUserDataById(user.id)
-
-        if (lastestData == null) {
-            println("존재하지 않는 유저입니다.")
+        if (user.orderHistory.isEmpty()) {
+            println(OrderHistoryMessage.ORDER_HISTORY_EMPTY)
             return
         }
 
-        if (lastestData.orderHistory.isEmpty()) {
-            println("주문 내역이 없습니다.")
-            return
-        }
-
-        lastestData.orderHistory.forEachIndexed { index, order ->
-            println("[${index + 1}] 주문번호: ${order.orderId}")
-            println("주문 날짜: ${order.orderDate}")
-            println("주문 항목:")
+        user.orderHistory.forEachIndexed { index, order ->
+            println(OrderHistoryMessage.ORDER_NUMBER_PREFIX.format(index + 1, order.orderId))
+            println(OrderHistoryMessage.ORDER_DATE_PREFIX.format(order.orderDate))
+            println(OrderHistoryMessage.ORDER_ITEMS_TITLE)
             order.orderItems.forEach {
                 println("${it.menuName} x ${it.quantity}")
             }
-            println("--------------------------------")
+            println(OrderHistoryMessage.ORDER_SEPARATOR)
         }
     }
 }
