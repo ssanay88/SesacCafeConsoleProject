b54891edecd8d321c534453ecec3e802b6c56213
yangsuhyuck <pplloo748@gmail.com>
2025-05-30 17:39:16 +0900

    fix/ConsoleInput 오류 코드 수정

--- src/main/kotlin/view/ConsoleInput.kt
+++ src/main/kotlin/view/ConsoleInput.kt
@@ -6,7 +6,7 @@ class ConsoleInput {
     companion object{
         private lateinit var scanner: Scanner
         fun consoleReadLine() : String {
-            if(this::scanner.isInitialized){
+            if(!this::scanner.isInitialized){
                 scanner = Scanner(System.`in`)
             }
             return scanner.nextLine()
