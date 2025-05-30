7f53b06fbd815c73913b0e05bad84723e09cbb84
yangsuhyuck <pplloo748@gmail.com>
2025-05-30 10:47:21 +0900

    fix/HomePage - CartManager 싱글턴 적용

--- src/main/kotlin/view/home/HomePage.kt
+++ src/main/kotlin/view/home/HomePage.kt
@@ -11,7 +11,6 @@ import viewmodel.cart.CartManager
 
 class HomePage() {
 
-    private val cartManager = CartManager()
     private val menuPage = MenuPage()
     private val orderPage = OrderPage()
     private val myPage = MyPage()
@@ -20,7 +19,7 @@ class HomePage() {
     fun startHomePage() {
         val nowUser = AuthManager.currentUser!!
         while (true) {
-            val isCartEmpty = cartManager.getItems(nowUser.id).isEmpty()
+            val isCartEmpty = CartManager.getItems(nowUser.id).isEmpty()
             if (isCartEmpty) {
                 print(HomeConstants.SHOW_HOME_MENU)
             } else {
@@ -29,7 +28,7 @@ class HomePage() {
 
             when (Input.inputZeroToMax(if (isCartEmpty) 3 else 4)) {
                 1 -> menuPage.startMenuPage(nowUser)
-                2 -> orderPage.startOrderPage(nowUser, cartManager.getItems(nowUser.id))
+                2 -> orderPage.startOrderPage(nowUser, CartManager.getItems(nowUser.id))
                 3 -> myPage.startMyPage(nowUser)
                 4 -> cartPage.startCartPage(nowUser)
                 0 -> return
